a2d0b915a670b5d9dce5e49d90439cb104e1f5e3
Go7hic <gtfx0209@qq.com>
2019-12-15 23:33:30 +0800

    first commit

---
+++ Sources/DeckOfPlayingCards/DeckOfPlayingCards.swift
@@ -0,0 +1,15 @@
+import DeckOfPlayingCards
+
+let numberOfCards = 10
+
+var deck = Deck.standard52CardDeck()
+deck.shuffle()
+
+for _ in 1...numberOfCards {
+    guard let card = deck.deal() else {
+        print("No More Cards!")
+        break
+    }
+
+    print(card)
+}
