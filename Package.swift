1eba93d2bccab9ca6ea85410f220d3dd2522d48a
Go7hic <gtfx0209@qq.com>
2019-12-15 23:54:05 +0800

    update

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DeckOfPlayingCards",
-            dependencies: ["DeckOfPlayingCards"]),
+            dependencies: ["Fisheryates", "PlayingCard"]),
         .testTarget(
             name: "DeckOfPlayingCardsTests",
             dependencies: ["DeckOfPlayingCards"]),
